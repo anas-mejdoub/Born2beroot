0ae1190dc959066f6f1140400b5431aabc8b190c
Anas mejdoub <144246471+anas-mejdoub@users.noreply.github.com>
2024-02-08 11:54:16 +0100

    first commit

---
+++ monitoring.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if dpkg -l | grep -q lvm; then
+    lvm_use="yes"
+else
+    lvm_use="no"
+fi
+arch=$(uname -a | awk '{gsub(/PREEMPT_DYNAMIC/, ""); print}')
+wall << EOF
+#Architecture: $(echo -n $arch)
+#CPU physical : $(lscpu | grep "CPU(s):" | head -n 1 | awk '{print $2}')
+#vCPU : $(grep -c ^processor /proc/cpuinfo)
+#Memory Usage : $(free -m | grep Mem | awk '{printf $3 "/" $2 "MB (%.2f%%)\n" ,($3/$2)*100}')
+#Disk Usage: $(df -m --total | grep total | awk '{printf $3 "/%.2fGb (%.2f)\n", $2/1024, ($3/$2)*100}')
+#Disk Usage: $(df -m | awk 'NR>1 {sum+=$3; sum2+=$2} END {printf sum "/%.2fGb (%.2f%%)\n", sum2/1024, (sum/sum2)*100}')
+#CPU load: $(mpstat | tail -n 1 |  awk '{print 100-$13"%"}')
+#Last boot: $(who | head -n 1 | awk '{print $3 " " $4}')
+#Lvm use: $lvm_us
+#Connection TCP : $(netstat | grep ESTABLISHED | wc -l) ESTABLISHED
+#User log: $(who | awk '{print $1}' | sort -u| wc -l)
+#Network: IP $(hostname -I) $(ip a | grep ether | awk '{print $2}')
+#Sudo : $(cat /var/log/sudo/sudo.log | grep COMMAND | wc -l) cmd
+EOF
