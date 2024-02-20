6222dc5f7669d16e51c31437aa5a50081e8dc292
Anas mejdoub <144246471+anas-mejdoub@users.noreply.github.com>
2024-02-20 15:33:35 +0100

    updated

--- monitoring.sh
+++ monitoring.sh
@@ -7,7 +7,7 @@ fi
 arch=$(uname -a | awk '{gsub(/PREEMPT_DYNAMIC/, ""); print}')
 wall << EOF
 #Architecture: $(echo -n $arch)
-#CPU physical : $(lscpu | grep "CPU(s):" | head -n 1 | awk '{print $2}')
+#CPU physical : $(lscpu | grep Socket | awk '{print $2}')
 #vCPU : $(grep -c ^processor /proc/cpuinfo)
 #Memory Usage : $(free -m | grep Mem | awk '{printf $3 "/" $2 "MB (%.2f%%)\n" ,($3/$2)*100}')
 #Disk Usage: $(df -m --total | grep total | awk '{printf $3 "/%.2fGb (%.2f)\n", $2/1024, ($3/$2)*100}')
@@ -17,6 +17,6 @@ wall << EOF
 #Lvm use: $lvm_us
 #Connection TCP : $(netstat | grep ESTABLISHED | wc -l) ESTABLISHED
 #User log: $(who | awk '{print $1}' | sort -u| wc -l)
-#Network: IP $(hostname -I) $(ip a | grep ether | awk '{print $2}')
+#Network: IP $(hostname -I) ($(ip a | grep ether | awk '{print $2}'))
 #Sudo : $(journalctl -q | grep sudo.*COMM | wc -l) cmd
 EOF
